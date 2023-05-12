d017f0dfbf2156fe590d5066042b20e3d5055433
JoMobile <lapinette2002@ymail.com>
2023-05-12 15:26:53 +0200

    ajout d'une fonction gradually_turn_off

--- Reaction_Chimique/reaction_chimique.py
+++ Reaction_Chimique/reaction_chimique.py
@@ -2,6 +2,8 @@ import machine, neopixel
 import math
 import time
 
+print("test")
+
 class LedStrip():
     def __init__(self, pin, band_length_in_meters, leds_per_meter):
         self.pin = pin
@@ -19,8 +21,14 @@ class LedStrip():
     
     def gradually_turn_on(self, color, delay):
         for i in range(self.number_of_leds):
-            self.set_strip_color(color)
             for j in range(i+1):
                 self.set_pixel_color(j, color)
+            self.show()
+            time.sleep_ms(delay)
+
+    def gradually_turn_off(self, delay):
+        for i in range(self.number_of_leds):
+            for j in range(i+1):
+                self.set_pixel_color(j, (0,0,0))
             self.show()
             time.sleep_ms(delay)
