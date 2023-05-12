d311765fd1f43c8ec1672ba7252c692629336609
JoMobile <lapinette2002@ymail.com>
2023-05-12 09:51:06 +0200

    création de la classe LedStrip pour faciliter le contrôle de plusieurs bandes

--- Reaction_Chimique/reaction_chimique.py
+++ Reaction_Chimique/reaction_chimique.py
@@ -1,15 +1,26 @@
 import machine, neopixel
+import math
+import time
 
-band_lenght_m = 10
-leds_per_meter = 30
+class LedStrip():
+    def __init__(self, pin, band_length_in_meters, leds_per_meter):
+        self.pin = pin
+        self.number_of_leds = math.floor(band_length_in_meters * leds_per_meter)
+        self.np = neopixel.NeoPixel(machine.Pin(pin), self.number_of_leds)
 
-number_of_leds = band_lenght_m * leds_per_meter
-pin = 5
-
-np = neopixel.NeoPixel(machine.Pin(pin), number_of_leds)
-
-np[0] = (255, 0, 0)
-np[3] = (125, 204, 223)
-np[7] = (120, 153, 23)
-np[10] = (255, 0, 153)
-np.write()
+    def set_pixel_color(self, index, color):
+        self.np[index] = color
+    
+    def set_strip_color(self, color):
+        self.np.fill(color)
+    
+    def show(self):
+        self.np.write()
+    
+    def gradually_turn_on(self, color, delay):
+        for i in range(self.number_of_leds):
+            self.set_strip_color(color)
+            for j in range(i+1):
+                self.set_pixel_color(j, color)
+            self.show()
+            time.sleep_ms(delay)
