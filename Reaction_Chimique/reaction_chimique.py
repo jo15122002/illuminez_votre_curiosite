b919e9abffc6a7591e52de4770e742adb3719968
JoMobile <lapinette2002@ymail.com>
2023-05-22 13:24:06 +0200

    Création de la fonction led adaptative

--- Reaction_Chimique/reaction_chimique.py
+++ Reaction_Chimique/reaction_chimique.py
@@ -2,32 +2,53 @@ import machine, neopixel
 import math
 import time
 
-print("test")
-
 class LedStrip():
-    def __init__(self, pin, band_length_in_meters, leds_per_meter):
+    def __init__(self, pin, number_of_leds, button_pin):
         self.pin = pin
-        self.number_of_leds = math.floor(band_length_in_meters * leds_per_meter)
+        # self.number_of_leds = math.floor(band_length_in_meters * leds_per_meter)
+        self.number_of_leds = number_of_leds
         self.np = neopixel.NeoPixel(machine.Pin(pin), self.number_of_leds)
+        self.button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_UP)
+        self.lastTick = time.ticks_ms()
 
-    def set_pixel_color(self, index, color, intensity=1):
-        self.np[index] = color * intensity
+        self.ledsOn = []
+        self.ledsOff = []
+        for i in range(self.number_of_leds):
+            self.ledsOff.append({
+                "id": i,
+                "intensity": 0,
+                "color": (0,0,0)
+            })
+        
+        self.set_strip_color((0,0,0), 1)
+        self.show()
+
+    def set_pixel_color(self, index, color, intensity=1.0):
+        if(intensity > 1.0):
+            intensity = 1.0
+        if(intensity < 0.0):
+            intensity = 0.0
+        self.np[index] = (int(color[0]*intensity), int(color[1]*intensity), int(color[2]*intensity))
     
-    def set_strip_color(self, color, intensity=1):
-        self.np.fill(color*intensity)
+    def set_strip_color(self, color, intensity=1.0):
+        if(intensity > 1.0):
+            intensity = 1.0
+        if(intensity < 0.0):
+            intensity = 0.0
+        self.np.fill((int(color[0]*intensity), int(color[1]*intensity), int(color[2]*intensity)))
     
     def show(self):
         self.np.write()
     
-    def gradually_turn_on(self, color, delay, intensity = 1):
+    def gradually_turn_on(self, color, delay, intensity = 1.0):
         for i in range(self.number_of_leds):
-            self.set_pixel_color(i, color)
+            self.set_pixel_color(i, color, intensity)
             self.show()
             time.sleep_ms(delay)
 
     def gradually_turn_off(self, delay):
-        for i in range(self.number_of_leds):    
-            self.set_pixel_color(self.number_of_leds - i, (0,0,0))
+        for i in range(self.number_of_leds):
+            self.set_pixel_color(self.number_of_leds - (i+1), (0,0,0))
             self.show()
             time.sleep_ms(delay)
 
@@ -40,12 +61,10 @@ class LedStrip():
                 "intensity": 0 - i*intensityStep,
                 "color": color
             })
-        while(ledsToRefresh[self.number_of_leds]["intensity"] < 1):
+        while(ledsToRefresh[self.number_of_leds-1]["intensity"] < 1):
             for led in ledsToRefresh:
-                self.set_pixel_color(led["id"], led["color"], led["intensity"])
                 led["intensity"] += intensityStep
-                if(led["intensity"] > 1):
-                    led["intensity"] = 1
+                self.set_pixel_color(led["id"], led["color"], led["intensity"])
             self.show()
             time.sleep_ms(delay)
 
@@ -54,22 +73,81 @@ class LedStrip():
         for i in range(self.number_of_leds):
             ledsToRefresh.append({
                 "id": i,
-                "intensity": 1 - i*intensityStep,
+                "intensity": 1.0 - i*intensityStep,
                 "color": color
             })
-        while(ledsToRefresh[self.number_of_leds]["intensity"] > 0):
+        while(ledsToRefresh[0]["intensity"] > 0):
             for led in ledsToRefresh:
-                self.set_pixel_color(led["id"], led["color"], led["intensity"])
                 led["intensity"] -= intensityStep
-                if(led["intensity"] < 0):
-                    led["intensity"] = 0
+                self.set_pixel_color(led["id"], led["color"], led["intensity"])
             self.show()
             time.sleep_ms(delay)
 
-    def wrong_answer_animation(self, color, delay):
+    def wrong_answer_animation(self, color, intensityStep, delay):
+        ledsToRefresh = []
+        index = 0
         for i in range(math.floor(self.number_of_leds/2)):
-            self.set_pixel_color(i, color)
+            ledsToRefresh.append({
+                "id": i,
+                "intensity": 0 - i*intensityStep,
+                "color": color
+            })
+        while(ledsToRefresh[math.floor(self.number_of_leds/2)-1]["intensity"] < 1):
+            for led in ledsToRefresh:
+                led["intensity"] += intensityStep
+                self.set_pixel_color(led["id"], led["color"], led["intensity"])
             self.show()
             time.sleep_ms(delay)
-        self.gradually_turn_off(delay)
+        
+        time.sleep_ms(1000)
+        
+        ledsToRefresh = []
+        for i in range(math.floor(self.number_of_leds/2)):
+            ledsToRefresh.append({
+                "id": i,
+                "intensity": 1.0 - i*intensityStep,
+                "color": color
+            })
+        while(ledsToRefresh[0]["intensity"] > 0):
+            for led in ledsToRefresh:
+                led["intensity"] -= intensityStep
+                self.set_pixel_color(led["id"], led["color"], led["intensity"])
+            self.show()
+            time.sleep_ms(delay)
+    
+    def adaptive_gradually_turn_on(self, color, delay, intensityStep):
+        if(time.ticks_ms() - self.lastTick > 10):
+            if(self.button.value() == 0):
+                # print("button pressed")
+                # print(len(self.ledsOff))
+                if(len(self.ledsOff) > 0):
+                    led = self.ledsOff[0]
+                    led["intensity"] += intensityStep
+                    led["color"] = color
+                    self.set_pixel_color(led["id"], led["color"], led["intensity"])
+                    self.show()
+                    # print(led["intensity"])
 
+                    if(led not in self.ledsOn):
+                        self.ledsOn.append(led)
+
+                    if(led["intensity"] >= 1):
+                        self.ledsOff.pop(0)
+                time.sleep_ms(delay)
+            else:
+                if(len(self.ledsOn) > 0):
+                    led = self.ledsOn[len(self.ledsOn)-1]
+                    led["intensity"] -= intensityStep
+                    led["color"] = color
+                    self.set_pixel_color(led["id"], led["color"], led["intensity"])
+                    self.show()
+                    if(led["intensity"] <= 0):
+                        self.ledsOff.insert(0, led)
+                        self.ledsOn.pop()
+                time.sleep_ms(delay)
+            self.lastTick = time.ticks_ms()
+
+led = LedStrip(23, 6, 21)
+
+while True:
+    led.adaptive_gradually_turn_on((255,0,0), 10, 0.1)
