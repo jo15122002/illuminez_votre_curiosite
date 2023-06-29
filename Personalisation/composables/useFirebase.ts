adc13545f4364d48a19b5bb95d7a3a4651e44f0c
Roxane-gll <guella.roxane@gmail.com>
2023-06-29 21:33:39 +0200

    Fix tweaks

--- Personalisation/composables/useFirebase.ts
+++ Personalisation/composables/useFirebase.ts
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 export const useFirebase = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyCfJwzhhqdATNZ7HPuBrYiYXqMmmCNeDZ4",
-        authDomain: "activite-3-biolumni.firebaseapp.com",
-        databaseURL: "https://activite-3-biolumni-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "activite-3-biolumni",
-        storageBucket: "activite-3-biolumni.appspot.com",
-        messagingSenderId: "954943036148",
-        appId: "1:954943036148:web:10db2c247d75ff11c01ca1"
+        // Your firebase config
     }
 
     const firebaseApp = initializeApp(firebaseConfig);
